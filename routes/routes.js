8538b99040ce537798eba082d14879e5ce71304e
hong0922learner <97691646+hong0922learner@users.noreply.github.com>
2022-03-18 20:20:10 +0800

    Add files via upload

---
+++ routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const signUpTempleteCopy = require('../models/SignupModels')
+
+router.post('/signup', (request, response) => {
+    const signedUpUser = new signUpTempleteCopy({
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
+    })
+    signedUpUser.save()
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        })
+})
+
+
+module.exports = router
